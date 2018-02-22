087da908fa9c5e21091d59ad135780e59177385c
Luca <l.zenobi@t4project.com>
2018-02-22 17:14:32 +0100

    Added log4j.properties

---
+++ src/main/java/city/gotham/security/DetectionApp.java
@@ -0,0 +1,31 @@
+package city.gotham.security;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class DetectionApp {
+
+    private static final Logger logger = LoggerFactory.getLogger(DetectionApp.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting service...");
+
+        final Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "DESKTOP-4GK2USK:9092");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        logger.info("Properties created...");
+
+        KafkaStreamsBuilder builder = new KafkaStreamsBuilder(config, "test");
+        KafkaStreams streams = builder.getKafkaStreams();
+        streams.start();
+    }
+
+}
--- src/main/java/gotham/security/DetectionApp.java
+++
@@ -1,9 +0,0 @@
-package gotham.security;
-
-public class DetectionApp {
-
-    public static void main(String[] args) {
-        System.out.println("Hello!");
-    }
-
-}
