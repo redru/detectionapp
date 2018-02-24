4437f0e43ba57ffcaad1f9c7be80b5f9923c80c0
Luca Zenobi <luzenobi@gmail.com>
2018-02-24 17:27:51 +0100

    Working on client Project refactor

---
+++ kafkaclient/src/main/resources/kafka-client/server.js
@@ -0,0 +1,19 @@
+"use strict";
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use('/', express.static('./app'));
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log('Listening at http://localhost:8080');
+});
