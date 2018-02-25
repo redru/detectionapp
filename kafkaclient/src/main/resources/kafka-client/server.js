0fe2acd4bb4b746c791a4275e6f87c0e7865f0a4
Luca Zenobi <luzenobi@gmail.com>
2018-02-25 03:47:07 +0100

    Completed frontend with sockets and kafka for nodejs

--- kafkaclient/src/main/resources/kafka-client/server.js
+++ kafkaclient/src/main/resources/kafka-client/server.js
@@ -1,19 +1,84 @@
 "use strict";
 const http = require('http');
+const kafka = require('kafka-node');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const client = new kafka.KafkaClient({ kafkaHost: 'DESKTOP-SBIL9SV:9092' });
+const client2 = new kafka.KafkaClient({ kafkaHost: 'DESKTOP-SBIL9SV:9092' });
+
+// Consumer login-topic
+const consumer = new kafka.Consumer(client, [{ topic: 'login-topic', offset: 0 }], { autoCommit: false, fromOffset: true });
+
+const loginTopicRows = [];
+
+consumer.on('message', message => {
+    // loginTopicRows.push(JSON.parse(message.value));
+    message = JSON.parse(message.value);
+
+    sockets.forEach(socket => socket.emit('new-login', message));
+});
+
+// Consumer login-failure-topic
+const consumer2 = new kafka.Consumer(client2, [{ topic: 'login-failure-topic' }], { autoCommit: false, fromOffset: false });
+
+consumer2.on('message', message => {
+    message = JSON.parse(message.value);
+
+    sockets.forEach(socket => socket.emit('login-failure', message));
+});
+
+// Producer
+const loginTopicProducer = new kafka.Producer(client);
+
 const app = express();
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use('/', express.static('./build'));
 
+app.post('/login', (req, res) => {
+    const now = new Date();
+
+    loginTopicProducer.send([
+        {
+            topic: 'login-topic',
+            attributes: 1,
+            messages: JSON.stringify({
+                logTime: `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`,
+                userID: req.body.user,
+                IP: '127.0.0.1',
+                status: req.body.status
+            })
+        }
+    ], (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+
+    res.status(200).json({ });
+});
+
+app.get('/topics/login', (req, res) => {
+    res.json({
+        rows: loginTopicRows
+    });
+});
+
+// SOCKETS CONFIGURATION
 const server = http.createServer(app);
 
+const io = require('socket.io')(server);
+const sockets = [];
+
+io.on('connection', socket => {
+    sockets.push(socket);
+});
+
+// SERVER STARTUP
 server.listen(8080, () => {
     console.log('Listening at http://localhost:8080');
 });
--- kafkaclient/src/main/resources/kafka-client/src/App.js
+++ kafkaclient/src/main/resources/kafka-client/src/App.js
@@ -1,21 +1,92 @@
+import $ from 'jquery';
+import io from 'socket.io-client';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loginTopicRows: [],
+            loginFailureTopicRows: []
+        };
+
+        this.onSubmitLogin = this.onSubmitLogin.bind(this);
+    }
+
+    componentDidMount() {
+        this.socket = io();
+
+        this.socket.on('new-login', message => {
+            this.setState({ loginTopicRows: this.state.loginTopicRows.concat(message) });
+        });
+
+        this.socket.on('login-failure', message => {
+            this.setState({ loginFailureTopicRows: this.state.loginFailureTopicRows.concat(message) });
+        });
+
+        $.get('/topics/login').done(data => {
+            this.setState({ loginTopicRows: data.rows });
+        });
+    }
+
+    onSubmitLogin(event) {
+        $.ajax({
+            method: 'POST',
+            url: '/login',
+            dataType: 'json',
+            data: { user: this.user.value, status: this.status.value }
+        }).done(() => {
+
+        }).fail(() => {
+            alert('error');
+        });
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <form onSubmit={this.onSubmitLogin}>
+                        <input type="text" placeholder="user" ref={(user) => this.user = user} /><br/>
+                        <select ref={(status) => this.status = status}>
+                            <option value="success">Success</option>
+                            <option value="fail">Fail</option>
+                        </select>
+                        <input type="submit" value="Login" />
+                    </form>
+                </header>
+                <p className="App-intro">
+                    <div id="loginTopicList" className="topic-list">
+                        <h3>Login Topic</h3>
+                        <ul>
+                            {this.state.loginTopicRows.map(this.renderLoginTopicRows)}
+                        </ul>
+                    </div>
+                    <div id="loginFailureTopicList" className="topic-list">
+                        <h3>Login Failure Topic</h3>
+                        <ul>
+                            {this.state.loginFailureTopicRows.map(this.renderLoginFailureTopicRows)}
+                        </ul>
+                    </div>
+                </p>
+            </div>
+        );
+    }
+
+    renderLoginTopicRows(row) {
+        return <li>[{row.logTime}] user: {row.userID} ({row.IP}) {row.status}</li>;
+    }
+
+    renderLoginFailureTopicRows(row) {
+        return <li>[{new Date().toDateString()}] user: {row.userID} - {row.message}</li>;
+    }
+
 }
 
 export default App;
